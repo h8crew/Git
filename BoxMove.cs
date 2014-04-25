fa7a179c08a3f32a1327e610321ac86631fee748
Christian McDonald <cmcdonald@nvyve.com>
2014-04-25 16:28:04 -0400

    Added Box 2

--- BoxMove.cs
+++ BoxMove.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class BoxMove : MonoBehaviour {
 
+	public GameObject cube;
 	public Camera mainCamera;
 	public float myFloat = 75f;
+	
+	void Start ()
+	{
+		cube.renderer.material.color = new Color(1f, 0f, 0f);
+	}
 
-	void Update () {
+	void Update ()
+	{
 		mainCamera.fieldOfView = myFloat;
 	}
 }
