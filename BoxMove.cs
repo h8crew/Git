91c8c8d1c3104058619a7c31c3adc2f100ae85c3
Christian McDonald <cmcdonald@nvyve.com>
2014-04-25 15:27:44 -0400

    1st commit

---
+++ BoxMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxMove : MonoBehaviour {
+
+	public Camera mainCamera;
+	public float myFloat = 75f;
+
+	void Update () {
+		mainCamera.fieldOfView = myFloat;
+	}
+}
